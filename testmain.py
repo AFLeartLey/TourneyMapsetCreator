43bc35ea2cd886cde6e22756cd95e8cea657b8de
AFLeartLey <2043118307@qq.com>
2026-03-08 17:48:28 +0800

    update fileprocessor

---
+++ testmain.py
@@ -0,0 +1,3 @@
+import util.osuFileProcessor as osuFileProcessor
+
+osuFileProcessor.editfile("testfile/891339 HyuN feat Ato - Asu wa Ame ga Yamukara/HyuN feat. Ato - Asu wa Ame ga Yamukara (Kalibe) [Hard].osu", "Qualifier", "NM1")
--- util/osuFileProcessor.py
+++ util/osuFileProcessor.py
@@ -1,6 +1,32 @@
-import regex
+import regex as re
 
-def editFileDiffname(filepath, name):
-    pass
+def editfile(filepath, roundname, slotname):
+    
+    keynames= ["Version", "Title", "TitleUnicode", "AudioFilename"]    
 
-def 
+    with open(filepath, "r", encoding="utf-8") as f:
+        content = f.read()
+
+    keys = "|".join(keynames)
+    pattern = rf"^({keys})(:\s*)(.*)$"
+
+    info = {}
+
+    for match in re.finditer(pattern, content, flags=re.MULTILINE):
+        key = match.group(1)   # Typename
+        val = match.group(3)   # Typeinfo
+        info[key] = val
+
+    bgpattern = r"\[Events\]\n(//Background and Video events)?\n(.*),(.*),\"(.*)\",(.*),(.*)"
+    bgmatch = re.search(bgpattern, content, flags=re.MULTILINE)
+    print(bgmatch)
+    if bgmatch:
+        info["Background"] = bgmatch.group(4)
+    else:
+        info["Background"] = ""
+
+    print("--- 提取到的原始信息 ---")
+    print(info)
+    
+
+
